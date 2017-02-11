c746851d7c90b3991e4f897cce310c775124644d
SudhakarReddyPeddinti <p.reddy.sudhakar@gmail.com>
2017-02-10 22:44:11 -0600

    Cleaned up word count example

--- Spark_WordCount_REST/src/main/scala-2.11/WordCount.scala
+++ Spark_WordCount_REST/src/main/scala-2.11/WordCount.scala
@@ -4,19 +4,12 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by sudhakar on 2/10/17.
   */
 object WordCount {
-
   def count(input_string:String): Array[(String, Int)] = {
-
     val sc = SparkContext.getOrCreate(new SparkConf().setAppName("WordCount").setMaster("local[*]"))
-
     val input = sc.parallelize(List(input_string))
-
-    val wc=input.flatMap(line=>{line.split(" ")}).map(word=>(word,1)).cache()
-
-    val output=wc.reduceByKey(_+_)
-
-    val res=output.collect()
-
+    val wc=input.flatMap(line=>{line.split(" ")}).map(word=>(word,1)).reduceByKey(_+_)
+    val res=wc.collect()
     res
   }
 }
+
